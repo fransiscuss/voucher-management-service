3daa827e95c2a3c463df702096ea0c88ef9c2553
Fransiscus Setiawan <fransiscus.setiawan@gmail.com>
2025-04-10 10:15:14 +1000

    feat: add messaging constants

---
+++ src/Application/Common/Constants/Messaging.cs
@@ -0,0 +1,6 @@
+namespace Acme.Services.VoucherManagementService.Application.Common.Constants;
+
+public static class Messaging
+{
+    public const string ServiceBusRedemptionTypePropertyKey = "RedemptionType";
+}
